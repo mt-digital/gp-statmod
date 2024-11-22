02b0c1ffa24eaa42c5c4c78947c3c5f6c3e0d456
Matt Turner <maturner01@gmail.com>
2024-11-22 09:50:29 -0800

    plot good enough for now

--- GroupPolarizationStatmod/scripts/plot.R
+++ GroupPolarizationStatmod/scripts/plot.R
@@ -11,6 +11,7 @@
 
 library(forcats)
 library(ggplot2)
+library(latex2exp)
 
 source("scripts/analysis.R")
 
@@ -30,7 +31,9 @@ plot_sigval_for_low_fwer = function(target_fwer = 0.05,
 }
 
 
-plot_fwer_fdr = function(probit_fits_dir = "data/probit_fits",
+plot_fwer_fdr = function(save_path = "paper/Figures/fwer_fdr_experiment.pdf", 
+                         aggregate = FALSE,
+                         probit_fits_dir = "data/probit_fits",
                          base_rate = 0.1, power = 0.8, 
                          significance_vals = c(0.2, 0.5, 0.8),
                          medium_sig_val = 0.5) {
@@ -38,7 +41,7 @@ plot_fwer_fdr = function(probit_fits_dir = "data/probit_fits",
   # Load data.
   dat = calc_fdr_vs_significance(significance_vals = significance_vals) %>%
     mutate(ExperimentID = paste(StudyID, ExperimentID, sep = "_")) %>%
-    select(!c(StudyID)) %>% 
+    # select(!c(StudyID)) %>% 
     mutate(SigVal = as_factor(SigVal))
 
   # Filter to keep just "medium" effect sizes and sort factors.
@@ -46,31 +49,25 @@ plot_fwer_fdr = function(probit_fits_dir = "data/probit_fits",
     filter(dat, SigVal == medium_sig_val) %>% 
     mutate(ExperimentID = fct_reorder(ExperimentID, FDR))
     
+  # Redefine default shape palette
+  scale_shape_discrete = function(...) {
+    scale_shape_manual(values = c(6, 1, 0))
+  }
+
   p = 
     ggplot(dat_med, aes(x = FDR, y = ExperimentID)) + 
-      geom_bar(stat = "identity", fill = "red", color = "red", width = 0.15)  +
-      geom_point(aes(x = FWER, y = ExperimentID), 
-                 dat %>% filter(SigVal == medium_sig_val), size = 2) + 
-      geom_point(aes(x = FDR, y = ExperimentID, shape = SigVal), dat) +
-      xlim(c(0, 1))
+      geom_bar(aes(fill = StudyID, color = StudyID), stat = "identity", width = 0.15)  +
+      labs(fill = "Study ID", color = "Study ID") +
+      # geom_point(aes(x = FWER, y = ExperimentID, color = StudyID, fill = StudyID), 
+      #            dat %>% filter(SigVal == medium_sig_val), size = 2, pch=21) + 
+      geom_point(aes(x = FDR, y = ExperimentID, shape = SigVal, color = StudyID), 
+                 dat, size = 2.25) + 
+      labs(shape = TeX('Significance, $d^*$')) +
+      xlim(c(0, 1)) + 
+      xlab("False detection rate (FDR)") + 
+      ylab("Experiment ID")
 
-      
-
-
-  # %>%
-  #   # Concatenate ExperimentID name.
-  #   mutate(ExperimentID = paste(StudyID, ExperimentID, sep = "_"), 
-  #          SigVal = as_factor(SigVal)) %>%
-  #   # arrange(desc(FWER)) %>%
-  #   ggplot(aes(x = FWER, y = ExperimentID, shape = SigVal)) + geom_point()
-    # Tall-ify table to have an FDR and FWER for each significance value.
-    # melt(id.vars = c("StudyID", "ExperimentID", "SigVal", "Power", "BaseRate"),
-    #      value.name = "Value", variable.name = "Measure") %>%
-    # Plot...
-    # ggplot(aes(x = Value, y = ExperimentID, color = SigVal, shape = Measure)) +
-    #   geom_point()
-
-  ggsave("test.pdf", p)
+  ggsave(save_path, p)
 
   return (p)
 }
